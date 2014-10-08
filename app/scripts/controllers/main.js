8c695044802098306dac3fefdeebb9f689a695aa
LE VAILLANT Pierre <pierrelv@live.fr>
2014-10-08 17:08:39 +0200

    - "angular-google-maps" directive added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,28 @@
  */
 angular.module('newYorkTimesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	
+  	var lat;
+  	var lng;
+
+	$scope.map = {
+		control: {},
+		center: {
+		latitude: 20,
+		longitude: -10
+		},
+		zoom: 3
+	};
+
+	// Set fullscreen for map
+	document.getElementsByClassName('angular-google-map-container')[0].style.height = window.innerHeight+'px';
+
+	// User geolocalisation 
+  	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			lat = position.coords.latitude;
+			lng = position.coords.longitude;
+		});
+  	}
+
   });
--- app/scripts/controllers/trends.js
+++ app/scripts/controllers/trends.js
@@ -8,10 +8,6 @@
  * Controller of the newYorkTimesApp
  */
 angular.module('newYorkTimesApp')
-  .controller('TrendsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('TrendsCtrl', function () {
+
   });
