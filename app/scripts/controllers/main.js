0f5fea74a83a33c4d75bdb5b84f36a0ad12bd65f
LE VAILLANT Pierre <pierrelv@live.fr>
2014-10-08 21:48:10 +0200

    - Map style added (Cool grey on Snazzy Maps)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,9 @@ angular.module('newYorkTimesApp')
 	
   	var lat;
   	var lng;
+  	$scope.position = {};
+  	$scope.position.latitude = 48.8689088;
+	$scope.position.longitude =  2.4502272;
 
 	$scope.map = {
 		control: {},
@@ -19,7 +22,14 @@ angular.module('newYorkTimesApp')
 		latitude: 20,
 		longitude: -10
 		},
-		zoom: 3
+		zoom: 3,
+		options: {
+			styles:[{'featureType':'landscape','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'transit','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'poi','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'water','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'stylers':[{'hue':'#00aaff'},{'saturation':-100},{'gamma':2.15},{'lightness':12}]},{'featureType':'road','elementType':'labels.text.fill','stylers':[{'visibility':'on'},{'lightness':24}]},{'featureType':'road','elementType':'geometry','stylers':[{'lightness':57}]}],
+			panControl:false,
+			scaleControl:false,
+			zoomControl:false,
+			streetViewControl:false
+		}
 	};
 
 	// Set fullscreen for map
@@ -30,7 +40,15 @@ angular.module('newYorkTimesApp')
 		navigator.geolocation.getCurrentPosition(function(position){
 			lat = position.coords.latitude;
 			lng = position.coords.longitude;
+			$scope.refreshMap(lat,lng,14);
 		});
   	}
+
+  	$scope.refreshMap = function (lat, lng, zoom) {
+    	//optional param if you want to refresh you can pass null undefined or false or empty arg
+	    $scope.map.control.refresh({latitude: lat, longitude: lng});
+	    $scope.map.control.getGMap().setZoom(zoom);
+	    return;
+  	};
 
   });
