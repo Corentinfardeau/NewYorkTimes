541050b5afe8f2789a4218b11e4e7feb3e5f10ee
LE VAILLANT Pierre <pierrelv@live.fr>
2014-11-14 11:33:43 +0100

    - Merging branch

--- app/scripts/directives/card.js
+++ app/scripts/directives/card.js
@@ -14,26 +14,38 @@ angular
       restrict: 'E',
       link: function postLink(scope) {
 
+
   			var width = document.getElementsByClassName('card-container')[0].offsetWidth+20;
    			var limit = width/2;
 
+   			//On dragmove event
        		scope.onHammer = function onHammer (event) {
 
+       			//if gesture is left
        			if(event.deltaX<=0)
        			{
+       				//Get the target of the event
 	       			var card = event.target.parentNode;
 
+	       			//As the class is not card, we go back into the DOM
 	       			while(!hasClass(card, 'card'))
-                    {
-                        card = card.parentNode;
-                    }
+		      			{
+		      				//We stock the container of the div card
+							card = card.parentNode;
+		      			}
 
+		      		//Get the remove div
 	      			var removeCard = card.nextSibling.nextSibling;
-	      			var svg = removeCard.querySelector('svg');
 
-	      			card.className = 'card';
-	      			removeCard.className = 'remove-card';
+	      			//Add the classes
+	      			card.classList.add('card');
+	      			removeCard.classList.add('remove-card');
 
+	      			//remove classes
+					card.classList.remove('smoothEffect');
+					removeCard.classList.remove('smoothEffect');
+
+	      			//Apply the style
 	      			card.style.webkitTransform = 'translateX('+event.deltaX+'px)';
 	      			card.style.MozTransform = 'translateX('+event.deltaX+'px)';	
 	      			card.style.msTransform = 'translateX('+event.deltaX+'px)';	
@@ -44,32 +56,46 @@ angular
 	      			removeCard.style.msTransform = 'translateX('+event.deltaX+'px)';	
 	      			removeCard.style.transform = 'translateX('+event.deltaX+'px)';
 
-	      			var opacity = -(event.deltaX/limit);
-	      			var opacity2 = (1-(-(event.deltaX/width)));
+	      			//calcul of the trash opacity
+	      			var svgOpacity = -(event.deltaX/limit);
+	      			
+	      			//calcul of the card opacity
+	      			var cardOpacity = (1-(-(event.deltaX/width)));
 
-	      			svg.style.opacity = opacity;
-	      			card.style.opacity = opacity2;
+
+	      			var svg = removeCard.querySelector('svg');
+
+	      			//Apply style
+	      			svg.style.opacity = svgOpacity;
+	      			card.style.opacity = cardOpacity;
 	      			
        			}
 
 			};
-
+			//On dragend event
 			scope.onHammerEnd = function onHammerEnd (event) {
 
+				//Get the target of the event
 				var card = event.target.parentNode;
 
+				//As the class is not card, we go back into the DOM
 				while(!hasClass(card, 'card'))
 				{
+					//We stock the container of the div card
 					card = card.parentNode;
 				}
 
+				//Get the remove div
 				var removeCard = card.nextSibling.nextSibling;
 
-				card.className = 'card smoothEffect';
-				removeCard.className = 'remove-card smoothEffect';
+				//Add classes
+				card.classList.add('smoothEffect');
+				removeCard.classList.add('smoothEffect');
 
+				//if the gesture is more than the middle of any screen
        			if(event.deltaX < -limit)
        			{
+     				//Apply style to remove the card
 	      			card.style.webkitTransform = 'translateX(-'+width*2+'px)';
 	      			card.style.MozTransform = 'translateX(-'+width*2+'px)';	
 	      			card.style.msTransform = 'translateX(-'+width*2+'px)';	
@@ -79,19 +105,30 @@ angular
 	      			removeCard.style.MozTransform = 'translateX(-'+width*2+'px)';	
 	      			removeCard.style.msTransform = 'translateX(-'+width*2+'px)';	
 	      			removeCard.style.transform = 'translateX(-'+width*2+'px)';
+
 	      			
       				var mobileContainer = document.getElementById('mobile-container');
+
+      				//Get the directive in the HTML
 	       			var cardDirective = card.parentNode.parentNode;
 
+	       			//remove all the card after the transition (0,3s)
 	      			setTimeout(function(){
 		       			
 		       			mobileContainer.removeChild(cardDirective);
+	      			},300);
 
-	      			}, 300);
+	      			setTimeout(function(){
+		       			
+		       			card.style.height = '0px';
+
+	      			},200);
+
        			}
+       			//if the gesture is less than the middle of any screen
        			else
-       			{
-
+       			{	
+       				//Apply style to not remove the card
 	      			card.style.webkitTransform = 'translateX(0px)';
 	      			card.style.MozTransform = 'translateX(0px)';	
 	      			card.style.msTransform = 'translateX(0px)';	
@@ -102,9 +139,16 @@ angular
 	      			removeCard.style.msTransform = 'translateX(0px)';	
 	      			removeCard.style.transform = 'translateX(0px)';
 
+	      			var svg = removeCard.querySelector('svg');
+
+	      			//Opacity to 1
+	      			svg.style.opacity = "1";
+	      			card.style.opacity = "1";
+
        			}
 			};
 
+			//fonction permettant de savoir si la classe existe
 			function hasClass(elem, className) {
 			    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 			}
