0a534ede8d7f32137658b6997425f6b7a91d1de5
Maxime DAGUET <maxime.daguet@gmail.com>
2014-11-22 21:24:55 +0100

    - About page updated - Link page updated - Bug fixed

--- app/scripts/directives/asidearticle.js
+++ app/scripts/directives/asidearticle.js
@@ -19,6 +19,7 @@ angular.module('newYorkTimesApp')
                 audio.volume=0.1;
                 audio.play();
                 document.querySelector('aside-article').classList.remove('aside--halfActive', 'aside--fullActive');
+                document.getElementById('toggleTwitter').innerHTML = 'Related tweets';
                 $rootScope.activeMarker.options.animation = 0;
                 $rootScope.map.center = {latitude: 34.833703,longitude: -41.768816};
                 $rootScope.map.zoom = 3;
@@ -30,6 +31,13 @@ angular.module('newYorkTimesApp')
             document.getElementById('toggleTwitter').addEventListener('click', function(e){  
                 e.preventDefault();
                 document.querySelector('aside-article').classList.toggle('aside--fullActive');
+                
+                if (document.querySelector('aside-article').classList.contains('aside--fullActive')) {
+                    document.getElementById('toggleTwitter').innerHTML = 'Close tweets';
+                } else {
+                    document.getElementById('toggleTwitter').innerHTML = 'Related tweets';
+                };
+                
             }, false);
       }
     };
